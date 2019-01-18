f863fbf7bf6cd68f4c0649716432aca30c520204
Mike Showman <mikeshowmanmca@gmail.com>
2019-01-18 12:57:33 -0500

    Staff commands on the way!

---
+++ Modules/Commands/say.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "say",
+    description: "Makes the bot say something!",
+    perm_lvl: ["OWNER", "STAFF"],
+    execute(message, tokens) {
+        if (tokens != undefined) {
+            let msg = "";
+            for (let i = 0; i < tokens.length; i++) {
+                msg = msg + tokens[i] + " ";
+            }
+            message.channel.send(msg);
+            message.delete()
+            .then (_msg => {
+                console.log(`Message successfully deleted in ${message.channel}.`);
+            });
+        } else {
+            message.channel.send(embeds.generateError(400));
+        }
+    },
+};
--- Modules/handler.js
+++ Modules/handler.js
@@ -80,69 +80,37 @@ function handleStaff(message) {
     // Message properties, allowing easier typing
     let content = message.content;
     let channel = message.channel;
-    let member = message.member;
     let author = message.author;
-    let guild = message.guild;
-
-    // Grab their roles, see if they're admin or owner of the guild
-    let admin = false;
-    if (author.id == guild.owner.id) {
-        admin = true;
-    } else {
-        let data = member.roles.array();
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].id == config.adminRole) {
-                admin = true;
-            }
-        }
-    }
 
     // Find the first space, or the end of line
     let command;
     let tokens;
     let firstSpace = content.indexOf(' ');
     if (firstSpace == -1) {
-        command = content;
+        command = content.substring(config.staffInvoke.length);
     } else {
-        command = content.substring(0, firstSpace);
+        command = content.substring(config.staffInvoke.length, firstSpace);
         tokens = content.substring(++firstSpace).split(' ');
     }
 
     // Make sure to log the command!
     console.log(`${author.username} (${author.id}) invoked command ${command} with tokens [${tokens}]`);
 
-    // Commands
-    if (command == "!modHelp") {
-        let msg = embeds.generateEmbed("I'm here to help!", "00ffff");
-        msg.addField("!say", "Allows the bot to \"say\" something.", true);
-        channel.send(msg);
-    } else if (command == "!say") {
-        if (tokens != undefined) {
-            let msg = "";
-            for (let i = 0; i < tokens.length; i++) {
-                msg = msg + tokens[i] + " ";
-            }
-            channel.send(msg);
-            message.delete()
-            .then (_msg => {
-                console.log(`Message successfully deleted in ${channel}.`);
-            });
+    // Commands execution
+    if (!client.commands.has(command)) {
+        message.channel.send(embeds.generateError(404));
+        return;
+    }
+
+    try {
+        if (!client.commands.get(command).perm_lvl.includes("STAFF")) {
+            message.channel.send(embeds.generateError(404));
         } else {
-            channel.send(embeds.generateError(400));
+            client.commands.get(command).execute(message, tokens);
         }
-    } else if (command == "!viewRoles") {
-        let staffRoles = member.roles.array();
-        let strStaffRoles = "";
-        for (let i = 0; i < staffRoles.length; i++) {
-            if (staffRoles[i].name == "@everyone") {
-                strStaffRoles = strStaffRoles + "everyone ";
-            } else {
-                strStaffRoles = strStaffRoles + staffRoles[i].name + " ";
-            }
-        }
-        channel.send(strStaffRoles);
-    } else {
-        channel.send(embeds.generateError(404));
+    } catch (error) {
+        console.log(error);
+        message.channel.send(embeds.generateError(0));
     }
 }
 
