1c6ea6c2c06bcf00f7adac065413c8d234a38c2f
Mike Showman <mikeshowmanmca@gmail.com>
2019-02-11 13:00:17 -0500

    Moves commands to a separate folder for organization

--- Modules/Commands/help.js
+++ Modules/Commands/help.js
@@ -5,15 +5,16 @@ module.exports = {
     async execute(message, tokens) {
         let msg = embeds.generateEmbed("I'm here to help!", "00ffff");
         let helpLine = "Find help on the website:";
+        let website = config.website + "/commands/";
 
         if (tokens.length != 0) {
             if (tokens[0].toLowerCase() == "owner" || tokens[0].toLowerCase() == "staff" || tokens[0].toLowerCase() == "general") {
-                msg.addField(helpLine, `${config.website}/${tokens[0].toLowerCase()}.html`);
+                msg.addField(helpLine, `${website}${tokens[0].toLowerCase()}.html`);
             } else {
                 msg.addField(helpLine, "Website not able to be found using that flag!");
             }
         } else {
-            msg.addField(helpLine, `${config.website}/general.html`);
+            msg.addField(helpLine, `${website}general.html`);
         }
         message.channel.send(msg);
     }
